53c316d1756f6e2e7c66f4b1a5437e0f6b73f512
AbduselamNur <99575092+AbduselamNur@users.noreply.github.com>
2023-10-10 08:18:12 +0300

    api

---
+++ api/src/users/dto/create-user.dto.ts
@@ -0,0 +1,25 @@
+import { Min } from "class-validator";
+import { IsNotEmpty, IsString, MinLength, IsEmail, MaxLength, IsPhoneNumber } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @MinLength(3)
+    @MaxLength(30)
+    @IsString()
+    name: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+
+    @IsNotEmpty()
+    @IsPhoneNumber()
+    @MinLength(10)
+    @MaxLength(10)
+    phone: string;
+}
